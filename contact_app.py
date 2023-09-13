9a17a9510925ab67570e90981d9d2bcbb78a1987
tejas <tejasshah@gmail.com>
2023-09-13 21:21:29 +0530

    list and search

--- contact_app.py
+++ contact_app.py
@@ -2,6 +2,27 @@
 
 contacts = []  # global list , in which we store all the contact details 
 
+c1 = {
+    "name":"ram","contactNum":"234"
+}
+
+c2 = {
+    "name":"ramanuj","contactNum":"234"
+}
+
+c3 = {
+    "name":"raone","contactNum":"234"
+}
+
+c4 = {
+    "name":"rock","contactNum":"234"
+}
+
+contacts.append(c1)
+contacts.append(c2)
+contacts.append(c3)
+contacts.append(c4)
+
 
 while True: 
     print("Press 1 For New Contact\nPress 2 For List\nPress 3 For Delete Contact\n4Press For Search Contact")
@@ -15,11 +36,20 @@ while True:
         c = {"name":name,"contactNum":contactNum}
         contacts.append(c)
     elif choice == 2:
-        print(contacts)
+       # print(contacts)#  [ {name,contact},{name,contact}]
+        print("NAME    CONTACT")
+        for c in contacts:
+            print(c.get("name"),"    ",c.get("contactNum"))#Ram 
+
     elif choice == 3:
         pass
     elif choice == 4:
-        pass 
+        print("Enter name for search")#ram
+        search = input() #ram 
+        for c in contacts:
+            if c.get("name").lower().startswith(search.lower()) :
+                print(c.get("name"),"    ",c.get("contactNum"))#Ram 
+
     elif choice == 0:
         exit(0)
     else:
